9bc4d3b89e5880fc127c92815b5303e7f9bea2ff
lucas-silva8077 <lucasfernando8077@gmail.com>
2026-09-09 20:59:15 -0300

    fix: corrigindo conflitos de merge por trabalho simultaneo em branchs diferentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import MainLayout from './components/MainLayout/Main'
-import { Outlet } from 'react-router'
 
 export default function App() {
   return (
-    <MainLayout>
-      <Outlet/>
-    </MainLayout>
+    <MainLayout />
   )
 }
