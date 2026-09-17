1cac211955fcf79b428ffb5f667c14997f89f800
PedroDaniel7 <02bouth@gmail.com>
2026-09-17 18:48:41 -0300

    fix: corrigir erros de build (props do MainLayout e import ausente no Footer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import MainLayout from './components/MainLayout/Main'
-import Home from './pages/Home'
 
 export default function App() {
   return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
+    <MainLayout />
   )
 }
