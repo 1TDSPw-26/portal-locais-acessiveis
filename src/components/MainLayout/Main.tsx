89b520d108775abd078b20d5848c83d745518bb3
PedroDaniel7 <02bouth@gmail.com>
2026-09-17 18:33:24 -0300

    fix: corrige erros de lint (import duplicado e variáveis não utilizadas)

--- src/components/MainLayout/Main.tsx
+++ src/components/MainLayout/Main.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import Header from './Header'
-import Footer from './Footer'
 
 export default function MainLayout() {
   return (
